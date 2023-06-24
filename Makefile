5026132facdf7001336e3f67cf639eaf73c840e2
Tata Pestova <tata.pestova@inbox.ru>
2023-06-24 17:45:20 +0300

    2 step

--- Makefile
+++ Makefile
@@ -5,3 +5,11 @@ run:
 start:
 
 		gunicorn task_manager.wsgi
+
+trans:
+
+		python manage.py makemessages -l ru -i .venv
+
+compile:
+
+		python manage.py compilemessages
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -124,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BACKEND = 'django.template.backends.django.DjangoTemplates'
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+    ]
